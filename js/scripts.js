482240e91c3950decdc6715839a4bfa1c6f133fc
Jared Clemmensen and Ron Craig <jared@>
2018-01-11 10:54:37 -0800

    add show all rides feature using CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-
+var totalInches;
 
   $("#formOne").submit(function(event) {
     var inches = parseInt($("input#inches1").val());
     var feet = parseInt($("input#feet1").val());
-    var totalInches = (feet * 12) + inches;
+    totalInches = (feet * 12) + inches;
     event.preventDefault();
+
+    $("div").removeClass("cantRide");
 
     if ( totalInches <= 36 && totalInches > 0 ) {
       $(".noRides").hide();
@@ -29,12 +31,35 @@ $(document).ready(function() {
       $(".kiddieRides").hide();
       $(".adultRides").hide();
     }
-
-
   });
 
-
-
+  $("button#show").click(function() {
+    console.log('clciked');
+    if ( totalInches <= 36 && totalInches > 0 ) {
+      $(".noRides").hide();
+      $("#swing").addClass("cantRide");
+      $(".kiddieRides").show();
+      $(".adultRides").addClass("cantRide");
+      $(".adultRides").show();
+    } else if ( totalInches > 36 && totalInches <= 48 ) {
+      $(".noRides").hide();
+      $(".kiddieRides").show();
+      $(".adultRides").addClass("cantRide").show();
+    } else if ( totalInches > 48 && totalInches <= 60) {
+      $(".noRides").hide();
+      $(".kiddieRides").show();
+      $(".adultRides").show();
+    } else if ( totalInches > 60 && totalInches <= 108) {
+      $(".noRides").hide();
+      $("#ferris").show();
+      $(".adultRides").show();
+      $("#swing").addClass("cantRide").show();
+    } else {
+      $(".noRides").hide();
+      $(".kiddieRides").addClass("cantRide").show();
+      $(".adultRides").addClass("cantRide").show();
+    }
+  });
 
 
 
