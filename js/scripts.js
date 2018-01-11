770bdb11d65d1b399065dfe96151421d66f073a2
Jared Clemmensen and Ron Craig <ron@>
2018-01-11 09:43:51 -0800

    add if branches, kid and adult rides

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
 
-var inches = parseInt(totalInches)
 
-///function totalInches(ft, in)
-  
+  $("#formOne").submit(function(event) {
+    var inches = parseInt($("input#inches1").val());
+    var feet = parseInt($("input#feet1").val());
+    var totalInches = feet * 12 + inches;
+    event.preventDefault();
+
+    if ( totalInches <= 36 && totalInches > 0 ) {
+      $("#ferris").show();
+    };
+
+    if ( totalInches >= 36 && totalInches <= 48 ) {
+      $(".kiddieRides").show();
+    };
+
+    if ( totalInches >= 48 && totalInches <= 60) {
+      $(".kiddieRides").show();
+      $(".adultRides").show();
+    }
+
+    if ( totalInches >= 60 && totalInches <= 108) {
+      $("#ferris").show();
+      $(".adultRides").show();
+    }
+
+
+  });
+
+
+
+
+
+
 });
